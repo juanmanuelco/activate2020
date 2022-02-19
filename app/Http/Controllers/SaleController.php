b818f1c44b1bfe5f7c0e5d4cd39b17d9a353d775
Juan Manuel Cuñez Olalla <juan.cunez@partiir.com>
2022-02-19 18:17:07 -0500

    change message on salecontroller

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -521,7 +521,7 @@ class SaleController extends Controller
             response()->json(['success' => true]);
         }catch (\Throwable $e){
             DB::rollBack();
-            return redirect()->back()->with('error', $e->getMessage() . ' in line '. $e->getLine());
+            abort(403, $e->getMessage() . ' in line '. $e->getLine() );
         }
     }
 }
