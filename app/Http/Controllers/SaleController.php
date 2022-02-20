eee72dbafee32a6be99a92b64dd16f4b89301d51
Juan Manuel Cuñez Olalla <juan.cunez@partiir.com>
2022-02-19 19:33:20 -0500

    Enviar roles al frontend

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -264,7 +264,7 @@ class SaleController extends Controller
 
 
         }
-        return view('pages.reports.sales')->with('cards', $cards);
+        return view('pages.reports.sales')->with(['cards'=> $cards, 'roles' =>$roles]);
     }
 
     public function  payer(Sale $sale){
